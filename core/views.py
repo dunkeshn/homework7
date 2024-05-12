bda35aeb540fc1f8b4fd48d07321f8987fc8b8d0
dunkeshn <serebrennikov48914@gmail.com>
2024-05-12 17:41:26 +0500

    init

---
+++ core/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from . import models
+
+def index(request):
+    news = models.News.objects.all()
+    return render(request, 'core/index.html', {'news': news, 'title': 'Список новостей'})
